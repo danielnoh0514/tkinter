98b799e15fdd67b2ab83e64f60ecb82265d86518
Daniel Noh <118356044+danielnoh0514@users.noreply.github.com>
2023-01-02 00:21:32 +0700

    Create Frame.py

---
+++ tkinterFunctions/Frame.py
@@ -0,0 +1,7 @@
+from tkinter import*
+
+root = Tk()
+root.title("tkinter")
+root.geometry('640x480+1000+400') # width * height + x axis + y axis
+root.resizable(False, False) # can't resize frame (width, height)
+root.mainloop()
